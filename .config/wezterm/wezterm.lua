a5746978ab2060d9527f08730f5952e9e7d076b5
Suman Kumar <sumankumar14963@gmail.com>
2024-10-08 12:59:48 +0530

    refactor(wezterm): single file configuration
    Change the configuration from two files ( .wezterm.lua and helpers.lua) to
    (.config/wezterm/wezterm.lua).

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,15 +1,11 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
-local module = {}
 
 local function private_helper()
 	wezterm.log_error("Hello !")
 end
 
-function module.apply_to_config(config)
-	private_helper()
-
-	config.colors = {
+return {
+	colors = {
 		foreground = "#CBE0F0",
 		background = "#011423",
 		cursor_bg = "#47FF9C",
@@ -19,35 +15,33 @@ function module.apply_to_config(config)
 		selection_fg = "#CBE0F0",
 		ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
 		brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
-	}
-	config.color_scheme = "Dracula"
+	},
 
-	config.font = wezterm.font("JetBrainsMono Nerd Font")
-	config.font_size = 19
+	color_scheme = "Dracula",
+	font = wezterm.font("JetBrainsMono Nerd Font"),
+	font_size = 19,
 
-	config.enable_tab_bar = false
+	enable_tab_bar = false,
 
-	config.window_decorations = "RESIZE"
-	config.window_background_opacity = 0.9
-	config.macos_window_background_blur = 10
-	config.window_background_gradient = {
+	window_decorations = "RESIZE",
+	window_background_opacity = 0.9,
+	macos_window_background_blur = 10,
+
+	window_background_gradient = {
 		orientation = "Vertical",
-
 		colors = {
 			"#223343",
 			"#000000",
 		},
 		interpolation = "Linear",
 		blend = "Rgb",
-	}
-
-	config.keys = {
+	},
+	keys = {
+		{ key = "f", mods = "CMD", action = wezterm.action.ToggleFullScreen },
 		{
-			key = "f",
-			mods = "CMD",
-			action = wezterm.action.ToggleFullScreen,
+			key = "|",
+			mods = "CTRL|ALT|SHIFT",
+			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
 		},
-	}
-end
-
-return module
+	},
+}
